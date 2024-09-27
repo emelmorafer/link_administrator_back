0086f7faba806d9aeaa0e2da561053193a715612
emelmorafer <emelmorafer@gmail.com>
2024-09-27 11:13:51 -0500

    A description of one category was fixed

--- enlaces/target/classes/data.sql
+++ enlaces/target/classes/data.sql
@@ -30,7 +30,7 @@ WHERE NOT EXISTS (SELECT 1 FROM categoria WHERE nombre = 'Youtuber Cinefilos');
 INSERT INTO categoria (nombre, descripcion, creationdate)
 SELECT
 'Youtuber Politicos',
-'Estos YouTubers producen videos que cubren una amplia variedad de aspectos deportivos, incluyendo noticias, análisis, comentarios, tutoriales y contenido entretenido relacionado con diferentes disciplinas deportivas.',
+'Es un creador de contenido en la plataforma YouTube que se enfoca en temas relacionados con la política. Su contenido abarca una amplia gama de tópicos políticos.',
 '2024-09-23 14:00:00'
 WHERE NOT EXISTS (SELECT 1 FROM categoria WHERE nombre = 'Youtuber Politicos');
 
@@ -38,7 +38,7 @@ WHERE NOT EXISTS (SELECT 1 FROM categoria WHERE nombre = 'Youtuber Politicos');
 INSERT INTO categoria (nombre, descripcion, creationdate)
 SELECT
 'Youtuber Deportivos',
-'Es un creador de contenido en la plataforma YouTube que se especializa en temas relacionados con el deporte.',
+'Estos YouTubers producen videos que cubren una amplia variedad de aspectos deportivos, incluyendo noticias, análisis, comentarios, tutoriales y contenido entretenido relacionado con diferentes disciplinas deportivas.',
 '2024-09-23 14:00:00'
 WHERE NOT EXISTS (SELECT 1 FROM categoria WHERE nombre = 'Youtuber Deportivos');
 
