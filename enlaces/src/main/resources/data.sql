153e18108cfb0d626459b91c4775c6a75e33dfa1
emelmorafer <emelmorafer@gmail.com>
2024-09-27 09:23:27 -0500

    A new category was added in the file data.sql

--- enlaces/src/main/resources/data.sql
+++ enlaces/src/main/resources/data.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS enlace (
 );
 
 
--- Estas 2 sentencias insertan los registros solo si no existen registros con el mismo nombre en la tabla categoria.
+-- Estas 3 sentencias insertan los registros solo si no existen registros con el mismo nombre en la tabla categoria.
 
 INSERT INTO categoria (nombre, descripcion, creationdate)
 SELECT
@@ -33,6 +33,14 @@ SELECT
 'Es un creador de contenido en la plataforma YouTube que se enfoca en temas relacionados con la política. Su contenido abarca una amplia gama de tópicos políticos.',
 '2024-09-23 14:00:00'
 WHERE NOT EXISTS (SELECT 1 FROM categoria WHERE nombre = 'Youtuber Politicos');
+
+
+INSERT INTO categoria (nombre, descripcion, creationdate)
+SELECT
+'Youtuber Deportivos',
+'Es un creador de contenido en la plataforma YouTube que se especializa en temas relacionados con el deporte. Estos YouTubers producen videos que cubren una amplia variedad de aspectos deportivos.',
+'2024-09-23 14:00:00'
+WHERE NOT EXISTS (SELECT 1 FROM categoria WHERE nombre = 'Youtuber Deportivos');
 
 
 
